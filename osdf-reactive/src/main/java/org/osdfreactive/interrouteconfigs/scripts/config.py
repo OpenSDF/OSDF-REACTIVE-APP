94056abb6797d3ee00b8a02fcdab998600578e2b
adibrastegarnia <adib.rastegarnia@gmail.com>
2017-12-22 16:56:56 -0500

    redundant files removed

--- osdf-reactive/src/main/java/org/osdfreactive/interrouteconfigs/scripts/config.py
+++ osdf-reactive/src/main/java/org/osdfreactive/interrouteconfigs/scripts/config.py
@@ -4,7 +4,7 @@ import os
 def config():
         os.system('curl --user onos:rocks -vX POST -H "Content-Type: application/json"'
                                 ' http://127.0.0.1:8181/onos/v1/network/configuration/ '
-                                '-d @/home/adib/osdf-reactive/src/main/java/org/osdfreactive/interrouteconfigs/scripts/config.json')
+                                '-d @~/OSDF-REACTIVE-APP/osdf-reactive/src/main/java/org/osdfreactive/interrouteconfigs/scripts/config.json')
 
 
 
