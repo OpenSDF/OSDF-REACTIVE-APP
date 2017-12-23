34b1f17f111b50b95a6a33dba93ebaefd290cde0
adibrastegarnia <adib.rastegarnia@gmail.com>
2017-12-23 14:18:54 -0500

    example 2 related files added

---
+++ osdf-reactive/src/main/java/org/osdfreactive/tools/mininet/example2/leafspineregions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# A simple test topology of three regions, A, B, and C.
+#
+# Script Configuration:
+#
+# host     : the controller instance against which this script is run
+
+host=${1:-127.0.0.1}
+
+
+###------------------------------------------------------
+### Start by adding the three regions A, B, and C
+
+# region-add <region-id> <region-name> <region-type> \
+#   <lat/Y> <long/X> <locType> <region-master>
+
+onos ${host} <<-EOF
+# -- define regions
+region-add siteA "siteA" LOGICAL_GROUP 30 20 grid ${host}
+EOF
+
+### Assign devices to each of their regions
+
+onos ${host} <<-EOF
+region-add-devices siteA \
+    of:0000000000000001 \
+    of:0000000000000002 \
+    of:0000000000000191 \
+    of:0000000000000192 \
+    of:0000000000000193 
+EOF
